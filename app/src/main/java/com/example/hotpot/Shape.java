760b9cdcb43e26e868fea3a4e9dbe37b0eb1ffd6
Mingzhen Yang <145230344+Mingzhen03@users.noreply.github.com>
2025-09-22 19:48:58 -0600

    add code

--- app/src/main/java/com/example/hotpot/Shape.java
+++ app/src/main/java/com/example/hotpot/Shape.java
@@ -3,4 +3,7 @@ package com.example.hotpot;
 public abstract class Shape {
     public int x;
     public int y;
+
+    // Demo: your local change to trigger a future merge conflict
+    public String color = "blue";
 }
