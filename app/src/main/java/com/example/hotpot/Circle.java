2ff06d3f54b52c87d737ce2ed48e85c6099e9dff
LimengChen11 <limeng1@ualberta.ca>
2025-09-22 19:49:01 -0600

    add code

--- app/src/main/java/com/example/hotpot/Circle.java
+++ app/src/main/java/com/example/hotpot/Circle.java
@@ -12,7 +12,7 @@ public class Circle extends Shape {
     public double area() {
         return Math.PI * radius * radius;
     }
-    
+
     public double perimeter() {
         return 2 * Math.PI * radius;
     }
