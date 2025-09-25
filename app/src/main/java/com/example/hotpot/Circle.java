35de6171ed8dd40d8d26a59993a9fda62d62c106
Cucu520 <yitong27@ualberta.ca>
2025-09-25 17:55:51 -0600

    text file added

--- app/src/main/java/com/example/hotpot/Circle.java
+++ app/src/main/java/com/example/hotpot/Circle.java
@@ -15,11 +15,9 @@ public class Circle extends Shape {
         this.radius = radius;
     }
 
-
     public double getArea() {
         return Math.PI * radius * radius;
     }
-
 
     public double getPerimeter() {
         return 2 * Math.PI * radius;
