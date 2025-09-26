435ff5ca959d0b7606b24d802fd2f6981eee2b9f
LimengChen11 <limeng1@ualberta.ca>
2025-09-25 18:00:11 -0600

    add code

--- app/src/main/java/com/example/hotpot/Circle.java
+++ app/src/main/java/com/example/hotpot/Circle.java
@@ -2,7 +2,7 @@ package com.example.hotpot;
 
 public class Circle extends Shape {
     private double radius;
-    
+
     public Circle(int x, int y, double radius) {
         this.x = x;   // inherited from Shape
         this.y = y;   // inherited from Shape
